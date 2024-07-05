1b532522e6d7219e3794e367705677d4cdbccaf2
carlosromanxyz <hola@carlosroman.xyz>
2024-07-05 00:25:51 -0400

    Local sync

---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import PageTemplate from "@/components/templates/PageTemplate";
+
+export default function HomePage() {
+  return (
+    <>
+      <PageTemplate />
+    </>
+  )
+}
---
+++ src/components/templates/PageTemplate.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/organisms/Header";
+import Footer from "@/components/organisms/Footer";
+
+export default function PageTemplate() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Page Template</h1>
+      </main>
+      <Footer />
+    </>
+  )
+}
